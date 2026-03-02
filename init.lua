31da6f64efbbdd52e49a83da02943c91ebadec96
Kavan Price <kavanprice@gmail.com>
2026-03-02 14:03:42 +0000

    Move to rustaceanvim

--- init.lua
+++ init.lua
@@ -860,24 +860,9 @@ require('lazy').setup({
   },
 
   {
-    'simrat39/rust-tools.nvim',
-    dependencies = { 'neovim/nvim-lspconfig' },
-    config = function()
-      local rt = require 'rust-tools'
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
-
-      rt.setup {
-        server = {
-          capabilities = capabilities,
-          on_attach = function(_, bufnr)
-            -- Hover actions
-            vim.keymap.set('n', '<C-space>', rt.hover_actions.hover_actions, { buffer = bufnr })
-            -- Code action groups
-            vim.keymap.set('n', '<Leader>a', rt.code_action_group.code_action_group, { buffer = bufnr })
-          end,
-        },
-      }
-    end,
+    'mrcjkb/rustaceanvim',
+    version = '^4',
+    ft = { 'rust' },
   },
 
   { -- Autoformat
@@ -1046,11 +1031,6 @@ require('lazy').setup({
   --     vim.cmd.colorscheme 'catppuccin'
   --   end,
   -- },
-
-  {
-    'simrat39/rust-tools.nvim',
-    dependencies = { 'neovim/nvim-lspconfig' },
-  },
   {
     'nvim-lua/plenary.nvim',
   },
