093df377952bb7059b83ec6a6596bd56a135d849
Kavan Price <kavanprice@gmail.com>
2026-07-30 15:12:45 +0100

    switch to zsh

--- init.lua
+++ init.lua
@@ -168,10 +168,7 @@ vim.opt.softtabstop = 4
 
 vim.opt.shortmess = vim.opt.shortmess + { c = true }
 vim.opt.signcolumn = 'yes'
-vim.opt.shell = 'nu'
-vim.opt.shellcmdflag = '-c'
-vim.opt.shellquote = ''
-vim.opt.shellxquote = ''
+vim.opt.shell = 'zsh'
 
 -- Language specific tabs
 vim.api.nvim_create_autocmd('FileType', {
